630be878a99a6ecbe7ec2e38e10ecf246a3f2f03
Sébastien Viande <sviande@gmail.com>
2021-06-10 11:58:34 +0200

    Main: restruct use channel for MatchEvent

--- main.go
+++ main.go
@@ -8,30 +8,46 @@ import (
 	"time"
 )
 
-func monitorMatches(previousMatches map[string]MatchInfo) map[string]MatchInfo {
+func updateMatches(matchEventChan chan MatchEvent, previousMatches map[string]MatchInfo) map[string]MatchInfo {
 	currentMatches, err := fetchMatches()
 	if err != nil {
-		log.Printf("Monitor failed, %v", err)
+		log.Printf("Fetch failed, %v", err)
+		return previousMatches
 	}
 
 	newEvents := compareNewInfos(currentMatches, previousMatches)
-	processEvents(newEvents)
+	for _, event := range newEvents {
+		matchEventChan <- event
+	}
 
 	return currentMatches
 }
 
-func processEvents(events []MatchEvent) {
-	if len(events) == 0 {
-		log.Println("Nothing new")
-		return
+func monitorMatches(matchEventChan chan MatchEvent) {
+	ticker := time.NewTicker(1 * time.Minute)
+	previousMatches := make(map[string]MatchInfo)
+	previousMatches = updateMatches(matchEventChan, previousMatches)
+	for {
+		select {
+		case <-ticker.C:
+			previousMatches = updateMatches(matchEventChan, previousMatches)
+		}
 	}
+}
 
-	for _, event := range events {
-		log.Printf("New event %d: label: %s\n", event.Event, event.Label)
+func sendSlackMessages(slack Slack, events chan MatchEvent) {
+	for event := range events {
+		log.Printf("Event %d: %s\n", event.Event, event.Label)
+		slack.sendMessage(event.Label)
 	}
 }
 
 func main() {
+	webHookURL := os.Getenv("WEBHOOK_URL")
+	if webHookURL == "" {
+		log.Fatal("Missing env var WEBHOOK_URL")
+	}
+
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
@@ -39,19 +55,16 @@ func main() {
 		os.Exit(0)
 	}()
 
-	previousMatches := make(map[string]MatchInfo)
+	slack := Slack{
+		hookURL: webHookURL,
+	}
+	slack.sendMessage("🔥Bot is up")
 
-	ticker := time.NewTicker(1 * time.Minute)
-	go func() {
-		previousMatches = monitorMatches(previousMatches)
-		for {
-			select {
-			case <-ticker.C:
-				previousMatches = monitorMatches(previousMatches)
-			}
-		}
-	}()
+	matchEventChan := make(chan MatchEvent, 10)
+	go monitorMatches(matchEventChan)
+	go sendSlackMessages(slack, matchEventChan)
 
 	<-c
+	close(matchEventChan)
 	log.Println("Exit")
 }
